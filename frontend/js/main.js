f6edccd2dfd4c96f95f8cd8e7aee88a3d5eb2d17
Jun Matsushita <jun@iilab.org>
2015-01-06 13:21:58 +0000

    Moved back to facetview and connected to elastic.iilab.org

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,15 @@
+jQuery(document).ready(function() {
 
+  $('.facet-view-simple').facetview({
+    search_url: 'https://elastic.iilab.org/noodles/document/_search?',
+    search_index: 'noodles',
+    datatype: 'json',
+    facets: [
+        {'field': 'source.source_label', 'size': 100, 'order':'term', 'display': 'publisher'},
+    ],
+    paging: {
+      size: 10
+    }
+  });
+
+});
